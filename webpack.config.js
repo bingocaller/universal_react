eb6f29fa580a79b7df5365e24d0438617d5eee46
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2016-01-12 11:16:47 +0100

    Fix Webpack pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 var path = require('path');
+var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var buildPath = path.join(process.cwd(), 'client-render.js');
 
 module.exports = {
   entry: {
-    app: path.join(process.cwd(), 'client-render.js'),
-    scss: './stylesheets/main.scss'
+    build: buildPath,
+    main: './stylesheets/main.scss'
   },
   output: {
     path: './public/',
-    filename: 'build.js'
+    filename: '[name].js',
+    chunkFilename: '[id].js'
   },
   devtool: 'source-map',
   module: {
     loaders: [
       {
-        test: /.js$/,
+        test: /.jsx?$/,
         loader: 'babel'
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!sass-loader?sourceMap')
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader!sass-loader?sourceMap')
       }
     ]
   },
+  postcss: function () {
+    return [autoprefixer];
+  },
   plugins: [
-    new ExtractTextPlugin('main.css', {
-      allChunks: true
-    })
+    new ExtractTextPlugin('[name].css')
   ]
 };
